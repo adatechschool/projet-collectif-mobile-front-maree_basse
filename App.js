5998d4453d70e24d28eacffcd134828ad3fd0976
Gaëtane SEGUIN <seguin.gaetane@gmail.com>
2024-05-02 11:55:03 +0200

    Tuto Expo

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+import * as ImagePicker from 'expo-image-picker';
+
+const PlaceholderImage = require('./assets/chat.jpg');
 
 export default function App() {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const pickImageAsync = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          allowsEditing: true,
+          quality: 1,
+        });
+
+        if (!result.canceled) {
+          setSelectedImage(result.assets[0].uri);
+          console.log(result);
+        } else {
+          alert('You did not select any image.');
+        }
+      };
+
   return (
     <View style={styles.container}>
-    <Text>Mon test </Text>
+        <View style={styles.imageContainer}>
+            <ImageViewer
+               placeholderImageSource={PlaceholderImage}
+               selectedImage={selectedImage}
+            />
+        </View>
+        <View style={styles.footerContainer}>
+            <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+            <Button label="Use this photo" />
+        </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +46,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#9ACD83',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+      flex: 1,
+      paddingTop: 100,
+    },
+ footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
