efd4d0af4920388faceae20424d3d24c8de9a79c
Gaëtane SEGUIN <seguin.gaetane@gmail.com>
2024-05-06 11:53:27 +0200

    Premier esai de JSON fonctionnel

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { StyleSheet, ScrollView, View } from 'react-native';
 import SpotView from './components/homePage/spot/SpotView.js';
-import records from './assets/spot.json';
+import Records from './assets/spot.json';
 
 const App = () => {
-console.log(records)
-const test = JSON.parse(records);
-console.log(test)
 
   return (
     <ScrollView style={styles.scrollView} >
-        <SpotView
-        image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Colombus_Isle.JPG/1200px-Colombus_Isle.JPG?20140713144334'
-        name="Venice Beach"
-        place="Los Angeles"/>
+        {Records.records.map((item,i) => (
+        <SpotView key={i}
+        image = {item.Photos}
+        name={item.Address}
+        />))}
     </ScrollView>
     )}
 
