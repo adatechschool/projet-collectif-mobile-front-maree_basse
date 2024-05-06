bc0d5c5f06c53c47b2b32898229ec9f9a9aaf7df
Gaëtane SEGUIN <seguin.gaetane@gmail.com>
2024-05-06 21:10:24 +0200

    ajout d'un useState pour l'asynchrone

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, ScrollView, View } from 'react-native';
 import SpotView from './components/homePage/spot/SpotView.js';
 import Records from './assets/spot.json';
@@ -15,41 +15,44 @@ var base = new Airtable({apiKey: API_TOKEN}).base('appEksYm9WhIjEtus');
 //console.log(Records.records[1].fields.Photos[0].url)
 //console.log(Records.records[0].fields.Address)
 
-let APIData=[]
-
 const App = () => {
-base('Surf Destinations').select({
-    // Selecting the first 3 records in By Surf Break:
-    maxRecords: 10,
-    view: "By Surf Break"
-}).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-    APIData=records
-    console.log(APIData)
-    records.forEach(function(record) {
+    const [APIData, setAPIData] = useState([]);
 
-    });
-    // To fetch the next page of records, call `fetchNextPage`.
-    // If there are more records, `page` will get called again.
-    // If there are no more records, `done` will get called.
-    fetchNextPage();
+    useEffect(() => {
+        console.log("UseEffect")
+        base("Surf Destinations").select({
+            // Selecting the first 3 records in By Surf Break:
+            maxRecords: 100,
+            view: "By Surf Break"
+        }).eachPage(function page(records, fetchNextPage) {
+            // This function (`page`) will get called for each page of records.
+            setAPIData(records);
+            console.log("test", APIData, records)
 
-}, function done(err) {
-    if (err) { console.error(err); return; }
-});
+            // To fetch the next page of records, call `fetchNextPage`.
+            // If there are more records, `page` will get called again.
+            // If there are no more records, `done` will get called.
+            fetchNextPage();
+        }, function done(err) {
+            if (err) { console.error(err); return; }
+        });
+    }, [])
+
+    console.log(APIData);
 
     return (
         <ScrollView style={styles.scrollView} >
-        {APIData.map((item,i)=>(
-            <SpotView key={i}
-                image = {item._rawJson.fields.Photos[0].url}
-                name = {item._rawJson.fields.Address}
-                />))}
-            </ScrollView>
-            )}
-
-
-
+        {
+            APIData.map((item,i)=> (
+                <SpotView key={i}
+                    image = {item._rawJson.fields.Photos[0].url}
+                    name = {item._rawJson.fields.Address}
+                />
+            ))
+        }
+        </ScrollView>
+    )
+}
 
 const styles = StyleSheet.create({
   container: {
