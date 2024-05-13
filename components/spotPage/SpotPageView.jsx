5a5843bef1d2444e0e744dd918ced7403f521a1d
EdithMenechi <edith.menechi@gmail.com>
2024-05-13 09:48:32 +0200

    améliorations visuelles

--- components/spotPage/SpotPageView.jsx
+++ components/spotPage/SpotPageView.jsx
@@ -4,6 +4,7 @@ import SpotViewModel from '../SpotViewModel';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useNavigation } from '@react-navigation/native';
+import { Linking } from 'react-native';
 
 
 const SpotPageView = (props) => {
@@ -15,19 +16,19 @@ const navigation = useNavigation();
    <Image style={styles.image} source={{uri:display.spot.image}}/>
    <Text style={styles.name}>{display.spot.name}</Text>
    <Text style={styles.title}>Place</Text>
-   <Text style={styles.place}>{display.spot.place}</Text>
+   <Text style={styles.grand}>{display.spot.place}</Text>
    <Text style={styles.title}>Difficulty</Text>
-   <Text style={styles.place}>{display.spot.difficulty}</Text>
+   <Text style={styles.grand}>{display.spot.difficulty}/5</Text>
    <Text style={styles.title}>Break Type</Text>
-   <Text>{display.spot.breaktype}</Text>
+   <Text style={styles.grand}>{display.spot.breaktype}</Text>
    <Text style={styles.title}>Peak Surf Season Begins</Text>
-   <Text>{display.spot.peakseasonbegins}</Text>
+   <Text style={styles.petit}>{display.spot.peakseasonbegins}</Text>
    <Text style={styles.title}>Peak Surf Season Ends</Text>
-   <Text>{display.spot.peakseasonends}</Text>
+   <Text style={styles.petit}>{display.spot.peakseasonends}</Text>
    <Text style={styles.title}>Geocode</Text>
-   <View style={styles.geocode}><Text>{display.spot.geocode}</Text></View>
+   <View style={styles.geocode}><Text style={styles.petit}>{display.spot.geocode}</Text></View>
    <Text style={styles.title}>Link</Text>
-   <Text style={styles.place}>{display.spot.link}</Text>
+   <Text style={styles.petit} onPress={() => Linking.openURL(display.spot.link)}>Discover the spot</Text>
    </ScrollView>
  );
 };
@@ -43,8 +44,6 @@ const styles = StyleSheet.create({
     fontSize: 25,
     fontWeight: 'bold',
     marginLeft: 12,
-
-
     },
   title: {
       color: 'grey',
@@ -53,22 +52,22 @@ const styles = StyleSheet.create({
       marginLeft:6,
       textTransform: 'uppercase',
     },
-
-      place: {
-          color: 'black',
-          fontSize: 18,
-          marginLeft:6,
-        },
-
-        geocode :{
-        Color:'black',
-        backgroundColor:'#e1ecf7',
-        fontSize: 18,
+  grand: {
+      color: 'black',
+      fontSize: 18,
+      marginLeft:6,
+    },
+  petit: {
+        color: 'black',
+        fontSize: 15,
         marginLeft:6,
-        marginRight:6,
-        borderRadius:8,
-
-        }
+      },
+  geocode :{
+      backgroundColor:'#e1ecf7',
+      marginLeft:6,
+      marginRight:6,
+      borderRadius:8,
+    },
 });
 
 export default SpotPageView;
